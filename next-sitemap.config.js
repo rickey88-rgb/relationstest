8fe4cb964d7503185ca2ed295590e69baa1997db
rickey88-rgb <rickey88@hotmail.se>
2026-09-04 22:31:20 +0200

    Fix sitemap domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.relationsvarning.se',
+  siteUrl: 'https://relationsvarning.se',
   generateRobotsTxt: true,
   sitemapSize: 5000,
 };
